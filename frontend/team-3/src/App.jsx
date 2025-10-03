90cd27f1a3758068894abcf5193515354a1d6d58
Karishma <karishmavijay21@gmail.com>
2025-10-03 11:33:18 +0100

    Create: login jsx and styling

--- frontend/team-3/src/App.jsx
+++ frontend/team-3/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./pages/Login.jsx";
 import ModelViewer from "./components/ModelViewer";
 function App() {
   const [count, setCount] = useState(0);
@@ -9,28 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <ModelViewer
-          //url="https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/main/2.0/ToyCar/glTF-Binary/ToyCar.glb"
-          url="./public/book.glb"   
-          width={400}
-          height={400}
-        />
-
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Login />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
