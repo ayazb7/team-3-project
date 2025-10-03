815853dee0c3a3f30912f172d7bb3bf12a9760e6
Panya <panya.rattanarom@sky.uk>
2025-10-03 15:10:39 +0100

    CREATE: add routing

--- frontend/team-3/src/App.jsx
+++ frontend/team-3/src/App.jsx
@@ -5,14 +5,18 @@ import "./App.css";
 import Login from "./pages/Login.jsx";
 import ModelViewer from "./components/ModelViewer";
 import LandingPage from "./pages/LandingPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="w-full h-full">
-        <LandingPage />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- frontend/team-3/src/components/Navbar.jsx
+++ frontend/team-3/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import { MenuIcon, HomeIcon } from "../assets";
+import { Link, Outlet } from "react-router-dom";
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [selectedOption, setSelectedOption] = useState(0);
 
   const options = [
-    { Home: <HomeIcon /> },
-    { About: <HomeIcon /> },
-    { Courses: <HomeIcon /> },
-    { Support: <HomeIcon /> },
+    { Label: "Home", Render: <HomeIcon />, Link: "/" },
+    { Label: "About", Render: <HomeIcon />, Link: "/about" },
+    { Label: "Courses", Render: <HomeIcon />, Link: "/login" },
+    { Label: "Support", Render: <HomeIcon />, Link: "/login" },
   ];
   return (
     <>
@@ -27,14 +28,20 @@ const Navbar = () => {
           </div>
 
           <div className="hidden md:flex flex-row w-full col-start-3">
-            <div className="flex items-center gap-3">
+            <nav className="flex items-center gap-3">
               {options.map((item, index) => {
-                const label = Object.keys(item)[0];
                 return (
-                  <div className="flex flex-row cursor-pointer ">{label}</div>
+                  <Link
+                    to={item["Link"]}
+                    key={index}
+                    className="flex flex-row cursor-pointer "
+                  >
+                    {item["Label"]}
+                  </Link>
                 );
               })}
-            </div>
+            </nav>
+            <Outlet />
           </div>
           <div className="hidden md:flex items-center justify-center col-start-5">
             <button className="!bg-blue-500 text-center !text-white ">
@@ -49,18 +56,17 @@ const Navbar = () => {
           } flex flex-col w-3/4 flex-1 bg-white shadow-lg p-5 gap-5`}
         >
           {options.map((item, index) => {
-            const [label, icon] = Object.entries(item)[0];
             return (
-              <div className="flex h-15 items-center ">
-                <div
-                  key={index}
+              <div key={index} className="flex h-15 items-center ">
+                <Link
+                  to={item["Link"]}
                   className={`flex flex-row h-full w-full p-2  rounded-sm items-center gap-2 ${
                     selectedOption == index ? "bg-blue-100" : ""
                   }`}
                 >
-                  {icon}
-                  {label}
-                </div>
+                  {item["Render"]}
+                  {item["Label"]}
+                </Link>
               </div>
             );
           })}
