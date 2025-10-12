9f037fd40dd3d41017e0c34af89c4328bff8bad9
Jibril <jibrilabdi01@gmail.com>
2025-10-12 20:30:58 +0100

    UPDATE Button.jsx styling

--- frontend/team-3/src/components/Button.jsx
+++ frontend/team-3/src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ className, label }) => {
   return (
     <button
-      className={`w-15 h-10  text-white !bg-blue-500  rounded-lg ${className}`}
+      className={`w-25 h-10  text-white !bg-blue-500  rounded-lg ${className}`}
     >
       {label}
     </button>
