561c6bf8f9c55dff8d0ac52fdd17f06a40ba178b
Panya <panya.rattanarom@sky.uk>
2025-10-16 21:51:24 +0100

    CREATE: create button

--- frontend/team-3/src/components/Button.jsx
+++ frontend/team-3/src/components/Button.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Button = ({ className, label }) => {
+const Button = ({ className, label, onClick }) => {
   return (
     <button
-      className={`w-15 h-10  text-white !bg-blue-500  rounded-lg ${className}`}
+      className={`w-15 h-10  text-white !bg-blue-500  rounded-lg ${className} cursor-pointer hover:!bg-blue-600`}
+      onClick={onClick}
     >
       {label}
     </button>
--- frontend/team-3/src/pages/Learning.jsx
+++ frontend/team-3/src/pages/Learning.jsx
@@ -3,9 +3,23 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useAuth } from "../context/AuthContext";
 
+const RenderOption = ({ label, onClick, roundDirection, className }) => {
+  return (
+    <span
+      className={`hover:bg-gray-300 cursor-pointer h-full w-full flex justify-center items-center ${
+        roundDirection === "left" ? "rounded-tl-lg" : "rounded-tr-lg"
+      } ${className}`}
+      onClick={onClick}
+    >
+      <p>{label}</p>
+    </span>
+  );
+};
+
 const Learning = () => {
   const [tutorialData, setTutorialData] = useState();
   const [courseData, setCourseData] = useState();
+  const [activeTab, setActiveTab] = useState(0);
   const { id } = useParams();
   const { accessToken } = useAuth();
   useEffect(() => {
@@ -35,20 +49,46 @@ const Learning = () => {
     fetchData();
   }, []);
   return (
-    <div className="flex flex-col justify-start items-start h-full w-full p-10 gap-5 text-sidebar-foreground !text-start">
-      <p className="">Dashboard / {courseData?.name} /</p>
+    <div className="flex flex-col justify-start items-start h-full w-full p-10 gap-5 text-sidebar-foreground !text-start overflow-scroll">
+      <p className="">Dashboard / {courseData?.name} </p>
       <div className="flex flex-col gap-2">
         <p className="text-black text-xl font-bold">{tutorialData?.title}</p>
-        <p>{tutorialData?.description}</p>
+        <p>Browse this tutorial</p>
       </div>
-      <div className="w-full h-96">
+      <div className="w-full h-1/2 shrink-0">
         <iframe
-          src="https://share.synthesia.io/embeds/videos/9e680982-8abe-4227-a96c-5906d2b71fbb"
+          src={tutorialData?.video_url}
           loading="lazy"
           title="Synthesia video player - How to create a strong password and stay safe online?"
           allow="encrypted-media; fullscreen;"
           className="w-full h-full"
         ></iframe>
+      </div>
+      <div className="w-full flex-grow h-auto flex flex-col bg-gray-200 rounded-lg">
+        <div className="grid grid-cols-2 text-center items-center h-10 divide-x divide-gray-400 shadow-lg shrink-0">
+          <RenderOption
+            label="Overview"
+            roundDirection="left"
+            onClick={() => setActiveTab(0)}
+            className={`${activeTab == 0 ? "bg-gray-300" : ""}`}
+          />
+          <RenderOption
+            label="Transcript"
+            roundDirection="right"
+            onClick={() => setActiveTab(1)}
+            className={`${activeTab == 1 ? "bg-gray-300" : ""}`}
+          />
+        </div>
+        <div
+          className={`${
+            activeTab == 0 ? "flex" : "hidden"
+          } flex-col w-full h-full p-5 gap-3`}
+        >
+          <p className="font-bold">Description</p>
+          <p>{tutorialData?.description}</p>
+          <p className="font-bold">Tutorial Category</p>
+          <p>{tutorialData?.category}</p>
+        </div>
       </div>
     </div>
   );
